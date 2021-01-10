a9bf7c988a5d7de45dacf60c94e14d86cfb1cc0e
Yugo Matsuda <yugo1997zazen@gmail.com>
2021-01-10 12:56:45 +0900

    reproduce issue

--- SmaplePBPopup/PopUpViewController.swift
+++ SmaplePBPopup/PopUpViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class PopUpViewController: UIViewController {
+    
+  
+    @IBOutlet var songImageView: UIImageView!
+    @IBOutlet var songTitle: UILabel!
+    @IBOutlet var artistTitle: UILabel!
+    
+    @IBOutlet var playButton: UIButton!
+    
+    var songImage: UIImage!
+    var songTitleString: String!
+    var artistTitleString: String!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        songImageView.layer.masksToBounds = false
+        songImageView.layer.shadowOpacity = 0.3
+        songImageView.layer.shadowRadius = 10
+        songImageView.layer.shadowColor = UIColor.black.cgColor
+        songImageView.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        songImageView.image = songImage
+        songTitle.text = songTitleString
+        artistTitle.text = artistTitleString
         // Do any additional setup after loading the view.
     }
     
--- SmaplePBPopup/ViewController.swift
+++ SmaplePBPopup/ViewController.swift
@@ -8,28 +8,36 @@
 import UIKit
 import PBPopupController
 class ViewController: UIViewController, PBPopupControllerDelegate, PBPopupBarDataSource {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Sample"
         // Do any additional setup after loading the view.
     }
     @IBAction func displayPopupAction(_ sender: Any) {
+        
         self.tabBarController?.popupController.delegate = self
-                
-                if let popupBar = self.tabBarController?.popupBar {
-
-                    popupBar.dataSource = self
-                    popupBar.title = "Title"
-                    
-                    let popupContentVC = PopUpViewController()
-                    
-                    self.tabBarController?.presentPopupBar(withPopupContentViewController: popupContentVC, animated: true, completion: {
-                        print("Presented")
-                    })
-                }
+        self.tabBarController?.popupContentView.popupIgnoreDropShadowView = true
+        if let popupBar = self.tabBarController?.popupBar {
+            
+            popupBar.dataSource = self
+            popupBar.title = "ray"
+            popupBar.subtitle = "BUMP OF CHICKEN"
+            
+            popupBar.image = UIImage(named:  "ray")
+            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let popupContentVC = storyboard.instantiateViewController(withIdentifier: "PopUpViewController") as? PopUpViewController
+            popupContentVC!.songImage = UIImage(named:  "ray")
+            popupContentVC!.songTitleString = "ray"
+            popupContentVC!.artistTitleString = "BUMP OF CHICKEN"
+            
+            self.tabBarController?.presentPopupBar(withPopupContentViewController: popupContentVC, animated: true, completion: {
+                print("Presented")
+            })
+        }
     }
     
-
+    
 }
 
